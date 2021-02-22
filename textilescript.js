b5a359f58a00e7a30c5248837bba4c3769281a51
xiiberpage <70550881+xiiberpage@users.noreply.github.com>
2021-02-21 22:05:35 -0600

    Add files via upload

---
+++ textilescript.js
@@ -0,0 +1,70 @@
+/*!
+ * Textile.js JavaScript Library v0.0.1
+ * https://textile.github.io
+ *
+ * Copyright 2016-2017 進擊的燊
+ * Released under the MIT license
+ *
+ * Date: 2016-04-03
+ */
+
+(function(window, undefined) {
+    var
+        doc = window.document,
+        defualtStyle = "display:inline-block;",
+        Textile = function(n) {
+            var
+                wordDiv,
+                charDiv;
+            this.words = [];
+            this.chars = [];
+            this.contentClone = n.innerHTML;
+            this.n = n;
+            n.innerHTML = '';
+            var wordsArr = this.contentClone.split(' ');
+            for (var i = 0, wordslen = wordsArr.length; i < wordslen;) {
+                wordDiv = doc.createElement('div');
+                wordDiv.style.cssText += defualtStyle;
+                var word = wordsArr[i++],
+                    charsArr = word.split('');
+                for (var j = 0, charslen = charsArr.length; j < charslen;) {
+                    charDiv = doc.createElement('div');
+                    charDiv.style.cssText += defualtStyle;
+                    charDiv.textContent = charsArr[j++];
+                    wordDiv.appendChild(charDiv);
+                    this.chars.push(charDiv);
+                }
+                n.appendChild(wordDiv);
+                this.words.push(wordDiv);
+                if (i !== wordslen) {
+                    wordDiv.insertAdjacentHTML('afterEnd', ' ');
+                }
+            }
+        }
+    Textile.prototype.revert = function() {
+        this.n.innerHTML = this.contentClone;
+    }
+    if (typeof window === "object" && typeof window.document === "object") {
+        window.Textile = window.T = Textile;
+    }
+})(window);
+/* ======================================== */
+
+var node = document.querySelector('h2 a');
+var text = new T(node);
+
+function random(min, max) {
+    return (Math.random() * (max - min)) + min;
+}
+
+text.chars.map(function(v, i){
+    TweenMax.from(v, 2.5, {
+        opacity: 0,
+        x: random(-500, 500),
+        y: random(-500, 500),
+        z: random(-500, 500),
+        scale: .1,
+        delay: i * .02,
+        yoyo: true
+    });
+});
