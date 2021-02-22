cd216d3da9ae62e43897299322aaa36654ce5689
xiiberpage <70550881+xiiberpage@users.noreply.github.com>
2021-02-22 13:35:09 -0600

    Add files via upload

---
+++ coronavirus_gamescript.js
@@ -0,0 +1,8 @@
+/* No JS!! 🤪 */
+
+/*
+    I’m CSS Architect and code evangelist at AnimaApp.com
+
+    You can see more of my stuff on my website eladsc.com
+    and follow me on twitter.com/eladsc
+*/
