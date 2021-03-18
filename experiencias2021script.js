6183fd461445989b341d0a9d4a633da5812d4dd7
xiiberpage <70550881+xiiberpage@users.noreply.github.com>
2021-03-17 18:27:54 -0600

    Delete experiencias2021script.js

--- experiencias2021script.js
+++
@@ -1,8 +0,0 @@
-// $ Xiiber Travel
-
-$(function() {
-    $('.event-list').find('.event').click(function() {
-        $('.event-list').find('.openEv').removeClass('open');
-        $(this).next().toggleClass('open');
-    })
-})
