34be1c7dca77678072ef1b0452fe7731bc8ed65f
xiiberpage <70550881+xiiberpage@users.noreply.github.com>
2021-03-17 18:38:09 -0600

    Delete coronavirus_gamescript.js

--- coronavirus_gamescript.js
+++
@@ -1,8 +0,0 @@
-/* No JS!! 🤪 */
-
-/*
-    I’m CSS Architect and code evangelist at AnimaApp.com
-
-    You can see more of my stuff on my website eladsc.com
-    and follow me on twitter.com/eladsc
-*/
