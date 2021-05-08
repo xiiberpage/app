192cbcc249967ae17b196d805d39594a88562d1b
xiiberpage <70550881+xiiberpage@users.noreply.github.com>
2021-05-08 12:23:31 -0500

    Delete whatsscript.js

--- whatsscript.js
+++
@@ -1,9 +0,0 @@
-/* 
-Adiciona/Remove a classe 'floating-aberto', 
-executando o efeito no menu flutuante
-*/ 
-$('#floating-icon').click(function(){
-  $('#floating-ligar').toggle();
-  $('#floating-whatsapp').toggle();
-  $('#floating-emergencia').toggle();
-});
