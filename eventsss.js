1355fe8b0fbe0244027bebf7e6598deb9b6f6d00
xiiberpage <70550881+xiiberpage@users.noreply.github.com>
2021-05-08 12:45:11 -0500

    Delete eventsss.js

--- eventsss.js
+++
@@ -1,7 +0,0 @@
-var container = document.querySelector('.article-container');
-var msnry = new Masonry( container, {
-  // options
-  itemSelector: '.article-brick',
-  gutter: 9,
-  isResizeBound: false
-});
