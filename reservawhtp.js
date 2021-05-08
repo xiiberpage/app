76f6de7a549bb9564e55a8fa0a4be72d60a2b32b
xiiberpage <70550881+xiiberpage@users.noreply.github.com>
2021-05-08 01:21:06 -0500

    Add files via upload

--- reservawhtp.js
+++ reservawhtp.js
@@ -23,7 +23,41 @@ formulario.addEventListener('submit', (event) => {
         let nombre = document.querySelector('#nombre').value
         let apellidos = document.querySelector('#apellidos').value
         let email = document.querySelector('#email').value
-        let mensaje = 'send?phone=' + telefono + '&text=*_Vestidor_*%0A*¿Cual es tu nombre?*%0A' + nombre + '%0A*¿Cuáles son tus apellidos?*%0A' + apellidos + '%0A*¿Cuál es tu correo electrónico?*%0A' + email + ''
+        let mensaje = 'send?phone=' + telefono + '&text=*_Reservación para Tepozspa del 17 abril con locker + desayuno_*%0A*Mi nombre es: *%0A' + nombre + '%0A*Fecha de cumpleaños: *%0A' + apellidos + '%0A*Email: *%0A' + email + ''
+        if (isMobile()) {
+            window.open(urlMobile + mensaje, '_blank')
+        } else {
+            window.open(urlDesktop + mensaje, '_blank')
+        }
+        buttonSubmit.innerHTML = '<i class="fab fa-whatsapp "></i> Enviar WhatsApp'
+        buttonSubmit.disabled = false
+    }, 3000);
+});
+$(".box-input input").on("focus", function() {
+    $(this).addClass("focus");
+});
+
+$(".box-input input").on("blur", function() {
+    if ($(this).val() == "") {
+        $(this).removeClass("focus");
+    }
+});
+
+const formulario = document.querySelector('#vestidor17abril2021');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://web.whatsapp.com/';
+const urlMobile = 'whatsapp://';
+const telefono = '5539793727';
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin "></i>'
+    buttonSubmit.disabled = true
+    setTimeout(() => {
+        let nombre = document.querySelector('#nombre').value
+        let apellidos = document.querySelector('#apellidos').value
+        let email = document.querySelector('#email').value
+        let mensaje = 'send?phone=' + telefono + '&text=*_Reservación para Tepozspa del 17 abril con locker _*%0A*Mi nombre es: *%0A' + nombre + '%0A*Fecha de cumpleaños: *%0A' + apellidos + '%0A*Email: *%0A' + email + ''
         if (isMobile()) {
             window.open(urlMobile + mensaje, '_blank')
         } else {
