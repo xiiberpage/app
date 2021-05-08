6a0257e1caaa1a2093405ffcb741dd9418d49769
xiiberpage <70550881+xiiberpage@users.noreply.github.com>
2021-05-08 13:20:22 -0500

    Delete topmenu.js

--- topmenu.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("mySidenavmenu").style.width = "250px";
-  document.getElementById("mainmenu").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenavmenu").style.width = "0";
-  document.getElementById("mainmenu").style.marginLeft= "0";
-}
